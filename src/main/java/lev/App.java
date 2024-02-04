6d439be2af645579f6c954897ab8263fcfb21e6b
kleonova <kleonova0@gmail.com>
2024-02-04 19:42:03 +0800

    process includeExt, ExcludeExt

--- src/main/java/lev/App.java
+++ src/main/java/lev/App.java
@@ -2,26 +2,23 @@ package lev;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.*;
 
 public class App {
     private static String directoryPath;
     private static boolean isRecursive = false;
     private static int maxDepth = 0;
     private static int countThreads = 1;
-    private List<String> includeExt = new ArrayList<>();
-    private List<String> excludeExt;
+    private static Set<String> includeExt = Collections.emptySet();
+    private static Set<String> excludeExt = Collections.emptySet();
     private static boolean useGitIgnore = false;
-    private List<String> formatReport;
+    private static Set<String> formatReport = Collections.emptySet();
 
     public static void main( String[] args ) throws Exception {
         initArgs(args);
         printArgs();
 
-        FileWalker fileWalker = new FileWalker(countThreads, maxDepth);
+        FileWalker fileWalker = new FileWalker(countThreads, maxDepth, includeExt, excludeExt);
         fileWalker.processCatalog(directoryPath);
         fileWalker.getReport().print();
     }
@@ -42,28 +39,65 @@ public class App {
             }
 
             if (args[i].contains("--max-depth")) {
-                maxDepth = Integer.parseInt(args[i].split("=")[1]);
+                maxDepth = getNumericArg(args[i]);
             }
 
             if (args[i].contains("--thread")) {
-                countThreads = Integer.parseInt(args[i].split("=")[1]);
+                countThreads = getNumericArg(args[i]);
+            }
+
+            if (args[i].contains("--include-ext")) {
+                includeExt = getSelectionArg(args[i]);
+            }
+
+            if (args[i].contains("--exclude-ext")) {
+                excludeExt = getSelectionArg(args[i]);
+            }
+
+            if (args[i].contains("--output")) {
+                formatReport = getSelectionArg(args[i]);
             }
         }
 
+        // если не задан флаг рекурсивно - отрабатывать только файлы в корне
         if (!isRecursive) {
             maxDepth = 1;
         }
+
+        // если конфликт includeExt и excludeExt ?
+    }
+
+    private static int getNumericArg(String argument) {
+        String argValues = argument.split("=")[1];
+        if (argValues.isEmpty() ) {
+            return 0;
+        }
+
+        return Integer.parseInt(argValues);
+    }
+
+    private static Set<String> getSelectionArg(String argument) {
+        String argValues = argument.split("=")[1];
+        if (argValues.isEmpty()) {
+            return Collections.emptySet();
+        }
+
+        return new HashSet<>(Arrays.asList(argValues.split(",")));
     }
 
     private static void printArgs() {
+        String useIncludeExt = includeExt.isEmpty() ? "не заданo" : includeExt.toString();
+        String useExcludeExt = excludeExt.isEmpty() ? "не заданo" : excludeExt.toString();
+        String useFormatReport = formatReport.isEmpty() ? "не заданo" : formatReport.toString();
+
         System.out.println("Параметры запуска:");
-        System.out.println("Путь " + directoryPath);
-        System.out.println("Запуск рекурсивно " + isRecursive);
-        System.out.println("Глубина обхода " + (maxDepth == 0 ? "не задана" : maxDepth));
-        System.out.println("Количество потоков для обхода " + countThreads);
-        System.out.println("Обрабатывать файлы с расширением " );
-        System.out.println("Не обрабатывать файлы с расширением ");
-        System.out.println("Не обрабатывать файлы, указанные в gitignore " + useGitIgnore);
-        System.out.println("Формат вывода ");
+        System.out.println("Путь: " + directoryPath);
+        System.out.println("Запуск рекурсивно: " + isRecursive);
+        System.out.println("Глубина обхода: " + (maxDepth == 0 ? "не задана" : maxDepth));
+        System.out.println("Количество потоков для обхода: " + countThreads);
+        System.out.println("Oбрабатывать файлы с расширением: " + useIncludeExt);
+        System.out.println("Не обрабатывать файлы с расширением: " + useExcludeExt);
+        System.out.println("Не обрабатывать файлы, указанные в gitignore: " + useGitIgnore);
+        System.out.println("Формат вывода: " + useFormatReport);
     }
 }
--- src/main/java/lev/FileWalker.java
+++ src/main/java/lev/FileWalker.java
@@ -1,29 +1,43 @@
 package lev;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class FileWalker {
-    private static final int TIMEWAIT = 120; // seconds
+    private static final int TIMEWAIT = 300; // seconds
     private Report report;
 
     private int maxDepth;
     private int countThreads;
 
-    private ExecutorService executor;
+    private Set<String> includeExt;
 
-    public FileWalker(int countThreads, int maxDepth) {
+    private Set<String> excludeExt;
+
+    private ExecutorService executor;
+    private Lock lock;
+
+    public FileWalker(int countThreads, int maxDepth, Set<String> includeExt, Set<String> excludeExt) {
         report = new Report();
         this.countThreads = countThreads;
         this.maxDepth = maxDepth;
+        this.includeExt = includeExt;
+        this.excludeExt = excludeExt;
     }
 
     public void processCatalog(String path) {
         executor = Executors.newFixedThreadPool(countThreads);
+        lock = new ReentrantLock();
 
         try {
             walk(Path.of(path));
@@ -47,7 +61,7 @@ public class FileWalker {
         Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-                if (root.relativize(dir).getNameCount() > maxDepth) {
+                if (root.relativize(dir).getNameCount() > maxDepth && maxDepth !=0) {
                     return FileVisitResult.SKIP_SUBTREE;
                 }
                 executor.submit(() -> {
@@ -73,6 +87,7 @@ public class FileWalker {
 
     // Обработка файла
     private void processFile(Path filePath) {
+        String extension = "";
         String filename = "";
         String absPath = "";
         long bytes = 0;
@@ -81,12 +96,26 @@ public class FileWalker {
             filename = filePath.getFileName().toString();
             absPath = filePath.toString();
             bytes = Files.size(filePath);
+            extension = filename.substring(filename.lastIndexOf(".") + 1);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+
+        boolean isProcessAsIncludeFile = includeExt.isEmpty() || includeExt.contains(extension);
+        boolean isProcessAsExcludeFile = excludeExt.isEmpty() || !excludeExt.contains(extension);
+        // проверить что файл попадает под обработку gitignore
+
+        if (!isProcessAsIncludeFile || !isProcessAsExcludeFile) {
+            System.out.println(filename + " не обрабатывать");
+            return;
+        }
+
         System.out.println("Обработка файла: " + filename + ", abs " + absPath + ", size " + bytes);
         ReportFile reportFile = new ReportFile(filename, absPath, bytes);
+
+        lock.lock();
         report.addFile(reportFile);
+        lock.unlock();
     }
 
     public Report getReport() {
